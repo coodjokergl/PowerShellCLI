99a65ba7fb60e67a9feccfdea70bed3aaa80f0e5
龚磊 <595459062@qq.com>
2020-07-16 14:09:08 +0800

    基础命令

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Powershell 命令文档
+
+
+```
+Powershell命令行学资料，从入门开始。
+```
+参考资料[PowerShell官网](!https://www.pstips.net/powershell-online-tutorials)
---
+++ src/01-基础知识/基本命令.ps1
@@ -0,0 +1,16 @@
+﻿
+# 计算结果
+function calcNumber{
+ return 1 * 2 * 3;
+}
+
+"计算结果：" 
+calc
+
+
+# 网络单位计算
+function calcNetSize{
+return 80kb * 1000 / 1gb;
+}
+"网络带宽："
+calcNetSize
